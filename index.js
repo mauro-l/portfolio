1e0e42ba478ea2e4a91c6e9e973e3d17a8bc95f5
Mauro Laime <maurol.dev@gmail.com>
2025-01-05 02:21:41 -0300

    fix: horizontal scroll removed from navbar menu on mobile

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const nav = document.getElementById("nav");
 const btnOpen = document.getElementById("open");
 const btnClose = document.getElementById("close");
+const navContainer = document.getElementById("navBox");
 
 btnOpen.addEventListener("click", () => {
   nav.classList.add("visible");
+  navContainer.style.overflowX = "visible";
 });
 
 btnClose.addEventListener("click", () => {
   nav.classList.remove("visible");
+  setTimeout(() => {
+    navContainer.style.overflowX = "hidden";
+  }, 400);
+});
+
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+    if (nav.classList.contains("visible")) {
+      nav.classList.remove("visible");
+    }
+  });
 });
